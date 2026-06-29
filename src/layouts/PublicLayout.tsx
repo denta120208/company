31d603d18176f722758e9fb227fe0316f4af5643
denta120208 <belajarit73@gmail.com>
2026-06-29 13:32:37 +0700

    fix: scroll to top on page navigation

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,8 +1,13 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 export default function PublicLayout() {
+  const { pathname } = useLocation()
+
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
